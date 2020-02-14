b5f7f75b9eb3143f5b3d3257140dff30ee3ae7d6
John Goiri <jg.goiri@gmail.com>
2020-02-13 16:16:33 -0800

    Implemented with std::vector, memory hog style

--- assignments/stack_implementation/stack.hpp
+++ assignments/stack_implementation/stack.hpp
@@ -1,40 +1,45 @@
-#include <list>
+#include <vector>
+#include <limits>
 
 class IntStack
 {
     public:
 
-        /// Returns true if there are no elements on the stack
-        bool empty() const
-        {
-            return this->stack_values.size()==0;
-        }
-
         /// Returns the number of elements on the stack
         int size() const
         {
-            return this->stack_values.size();
+            return this->top_index+1;
+        }
+
+        /// Returns true if there are no elements on the stack
+        bool empty() const
+        {
+            return this->size()==0;
         }
 
         /// Return the top element of the stack, i.e. the last element that was pushed
         int top() const
         {
-            return this->stack_values.front();
+            return this->stack_values.at(this->top_index);
         }
 
         /// Adds a new element to the top of the stack
         void push(const int& new_value)
         {
-            return this->stack_values.push_front(new_value);
+            ++top_index;
+            stack_values[top_index]=new_value;
+            return;
         }
 
         /// Remove the topmost element of the stack
         void pop()
         {
-            return this->stack_values.pop_front();
+            --top_index;
+            return;
         }
 
     private:
 
-        std::list<int> stack_values;
+        std::vector<int> stack_values=std::vector<int>(std::numeric_limits<int>::max());
+        int top_index=-1;
 };

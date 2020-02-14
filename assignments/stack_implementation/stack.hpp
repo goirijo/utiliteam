81d6f311e76cce998102bd8fa6722a4027733f6b
John Goiri <jg.goiri@gmail.com>
2020-02-13 16:07:45 -0800

    Implemented with std::list

--- assignments/stack_implementation/stack.hpp
+++ assignments/stack_implementation/stack.hpp
@@ -1,21 +1,40 @@
+#include <list>
+
 class IntStack
 {
     public:
 
         /// Returns true if there are no elements on the stack
-        bool empty() const;
+        bool empty() const
+        {
+            return this->stack_values.size()==0;
+        }
 
         /// Returns the number of elements on the stack
-        int size() const;
+        int size() const
+        {
+            return this->stack_values.size();
+        }
 
         /// Return the top element of the stack, i.e. the last element that was pushed
-        int top() const;
+        int top() const
+        {
+            return this->stack_values.front();
+        }
 
         /// Adds a new element to the top of the stack
-        void push(const int& new_value);
+        void push(const int& new_value)
+        {
+            return this->stack_values.push_front(new_value);
+        }
 
         /// Remove the topmost element of the stack
-        void pop();
+        void pop()
+        {
+            return this->stack_values.pop_front();
+        }
 
     private:
+
+        std::list<int> stack_values;
 };

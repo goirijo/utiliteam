2b36135d3391bdaef0fe115ebaa259eb963b41d0
Muna Saber <munasaber@ucsb.edu>
2020-02-17 14:57:22 -0800

    I can't believe it (almost) worked

--- assignments/stack_implementation/stack.hpp
+++ assignments/stack_implementation/stack.hpp
@@ -1,21 +1,34 @@
+#include <stack>
+
 class IntStack
 {
     public:
 
         /// Returns true if there are no elements on the stack
-        bool empty() const;
+        bool empty() const{my_stack.empty();};
 
         /// Returns the number of elements on the stack
-        int size() const;
+        int size() const{my_stack.size();};
 
         /// Return the top element of the stack, i.e. the last element that was pushed
-        int top() const;
+        int top() const{my_stack.top();};
 
         /// Adds a new element to the top of the stack
-        void push(const int& new_value);
+        void push(const int& new_value){my_stack.push(new_value);};
 
         /// Remove the topmost element of the stack
-        void pop();
+        void pop(){my_stack.pop();};
 
     private:
+	std::stack<int> my_stack;
+
 };
+//bool IntStack::empty()
+//{
+
+//}
+
+//int Instack::size()
+//{
+	
+//}

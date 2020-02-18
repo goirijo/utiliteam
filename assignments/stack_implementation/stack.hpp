57c1970ac09539c59fa79360c9d4c9b92da3e605
Muna Saber <munasaber@ucsb.edu>
2020-02-17 17:56:53 -0800

    Redid

--- assignments/stack_implementation/stack.hpp
+++ assignments/stack_implementation/stack.hpp
@@ -1,26 +1,31 @@
-#include <stack>
+#include <vector>
 
 class IntStack
 {
     public:
 
         /// Returns true if there are no elements on the stack
-        bool empty() const{my_stack.empty();};
+        bool empty() const{
+		my_vec.empty();};
 
         /// Returns the number of elements on the stack
-        int size() const{my_stack.size();};
+        int size() const{
+		my_vec.size();};
 
         /// Return the top element of the stack, i.e. the last element that was pushed
-        int top() const{my_stack.top();};
+        int top() const{
+		my_vec.front();};
 
         /// Adds a new element to the top of the stack
-        void push(const int& new_value){my_stack.emplace(new_value);};
+        void push(const int& new_value){
+		my_vec.emplace(my_vec.begin(),new_value);};
 
         /// Remove the topmost element of the stack
-        void pop(){my_stack.pop();};
+        void pop(){
+		my_vec.pop_back();};
 
     private:
-	std::stack<int> my_stack;
+	std::vector<int> my_vec;
 
 };
 //bool IntStack::empty()

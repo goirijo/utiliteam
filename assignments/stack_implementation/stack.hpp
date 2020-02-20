b337dcfeccee45e39d543ea5d39baf5c1747df57
Muna Saber <munasaber@ucsb.edu>
2020-02-20 13:56:25 -0800

    Finished code, passes most tests;

--- assignments/stack_implementation/stack.hpp
+++ assignments/stack_implementation/stack.hpp
@@ -10,15 +10,19 @@ class IntStack
 
         /// Returns the number of elements on the stack
         int size() const{
-		my_vec.size();};
+		my_vec.size();
+	};
 
         /// Return the top element of the stack, i.e. the last element that was pushed
         int top() const{
-		my_vec.front();};
+		my_vec.front();
+	};
 
         /// Adds a new element to the top of the stack
         void push(const int& new_value){
-		my_vec.emplace(my_vec.begin(),new_value);};
+		//my_vec.emplace(my_vec.begin(),new_value);
+		my_vec.emplace_back(new_value);
+	};
 
         /// Remove the topmost element of the stack
         void pop(){

b5874fa4cb51f0848eb8705361151070148e6a83
rcolleen <creynolds@ucsb.edu>
2020-05-05 10:09:28 -0700

    point group updates

--- projects/avdv-factor-group/tests.hpp
+++ projects/avdv-factor-group/tests.hpp
@@ -4,7 +4,10 @@
 #include "../../submodules/eigen-git-mirror/Eigen/Core"
 #include "../../submodules/eigen-git-mirror/Eigen/Dense"
 #include <iostream>
+#include <vector>
+
 void EXPECT_EQ_MATRICES(Eigen::Matrix3d matrix1, Eigen::Matrix3d matrix2, std::string test_name);
 void EXPECT_TRUE(bool is_true, std::string test_name);
 Eigen::Matrix3d make_z_rotation_matrix(double degrees);
+void EXPECT_EQ_vectorsD(std::vector<double> vector_1, std::vector<double> vector_2, std::string test_message);
 #endif

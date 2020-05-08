ed458573a090fc74f8914ff6bd97311ed4c13510
Muna Saber <munasaber@ucsb.edu>
2020-05-07 19:10:07 -0700

    Small changes

--- projects/avdv-factor-group/compile.sh
+++ projects/avdv-factor-group/compile.sh
@@ -2,3 +2,4 @@ g++ --std=c++17 lattice.cpp tests.cpp coordinate_test.cpp coordinate.cpp -o coor
 g++ --std=c++17 lattice.cpp tests.cpp lattice_test.cpp -o lattice_test
 g++ --std=c++17 symop.cpp lattice.cpp coordinate.cpp tests.cpp site_test.cpp site.cpp -o site_test
 g++ --std=c++17 symop.cpp lattice.cpp coordinate.cpp site.cpp tests.cpp structure_test.cpp structure.cpp -o structure_test
+g++ --std=c++17 symop.cpp tests.cpp symop_test.cpp symop.cpp -o symop_test

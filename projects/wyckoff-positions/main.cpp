b93896a5a6d05302e7a27d62377bc901cdb24c4a
rcolleen <creynolds@ucsb.edu>
2020-05-26 11:45:03 -0700

    wycoff outline and tests

---
+++ projects/wyckoff-positions/main.cpp
@@ -0,0 +1,14 @@
+#include stuff
+
+int main(){
+    /*
+    read in poscar
+    generate factor group
+    genreate subgroups
+    for each subgroup:  
+        find coset given subgroup and factor group
+        find invariant subspace(1 wyckoff position)
+        find all symmetrically equivalent (1 wyckoff*coset)
+     print wyckoff position
+        */
+}
---
+++ projects/wyckoff-positions/wyckoff.hpp
@@ -0,0 +1,56 @@
+#ifndef WYCOFF_HH
+#define WYCOFF_HH
+
+#include "../../submodules/eigen-git-mirror/Eigen/Core"
+#include "../avdv-factor-group/"
+#include <vector>
+
+
+class?? Wyckoff??
+    vector of vectors
+/*
+ * Some notes:
+ *  Wyckoff positions are vectors with some variable components.
+ *  they are points lines or planes? how do we define each?
+ *  ex: axis(x,0,0), pt(1/4, 14/, 1/4) plane(-z,1/2y, 0)
+ *
+ *  wyckoff positions are vectors of functions of new class called wyckoff function
+ *      ex:
+ *      wyxcoff_func(x,y,z){
+ *          double a=0;
+ *          double b=x-y;
+ *          double c=0;
+ *          }
+ *       wyckoff2(x,y,z){
+ *        double a=1;
+ *        double b=0;
+ *        double c=.5;
+ *        }
+ *find_all_equivalent_site({x,y,z}, wyckoff_1i, coset)
+    {
+        is_valid_wyckoff(wyckoff_1, {x,y,z})
+    for( symop : coset){
+            coset*{x,y,z}
+            }
+       i}
+i*/
+
+std::vector<SymOps> find_coset(SymGroup<SymOp, BinarySymOpPeriodicCompare_f, BinarySymOpPeriodicMultiplier_f> factor_group, SymGroup<SymOp, BinarySymOpPeriodicCompare_f, BinarySymOpPeriodicMultiplier_f> subgroup);
+
+Eigen::Matrix4d make_symop_4dmatrix(SymOp input_op);
+
+Eigen::Matrix4d average_symgroup_operations(SymGroup<SymOp, BinarySymOpPeriodicCompare_f, BinarySymOpPeriodicMultiplier_f> subgroup);
+
+Mystery_WyckoffType find_invariant_subspace(SymGroup<SymOp, BinarySymOpPeriodicCompare_f, BinarySymOpPeriodicMultiplier_f> subgroup);
+
+std::vector<Mystery_WyckoffType> find_symmetrically_equivalent_wyckoff_positions(std::vector<SymOp> coset, Mystery_wyckoffTpye wyckoff_position);
+
+
+
+
+
+
+
+
+
+#endif

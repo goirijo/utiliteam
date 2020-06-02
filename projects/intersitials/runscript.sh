035570c7440b7cf63600d11e2e49cb8e4e73bdbf
Muna Saber <munasaber@ucsb.edu>
2020-06-02 00:13:47 -0700

    Find interstitials within radius workded!

--- projects/intersitials/runscript.sh
+++ projects/intersitials/runscript.sh
@@ -1,2 +1,3 @@
-g++ interstitials.cpp xtal_classes.cpp symmetry_operations.cpp tests.cpp -o run
+#g++ interstitials.cpp xtal_classes.cpp symmetry_operations.cpp tests.cpp -o run
+g++ --std=c++17 ../avdv-factor-group/lattice.cpp ../avdv-factor-group/site.cpp ../avdv-factor-group/coordinate.cpp ../avdv-factor-group/symop.cpp ../avdv-factor-group/structure.cpp ../avdv-factor-group/tests.cpp interstitial_mesh.cpp interstitial_mesh_test.cpp -o interstitial_test
 exit 0
